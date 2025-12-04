ad10ea69b73c3ec19f17dbc02d39fea48a10eac4
axelfrache <axel.frache@etu.umontpellier.fr>
2025-12-04 20:14:25 +0100

    feat: Initial commit

---
+++ backend/src/main/java/dev/royaumendi/talentmap/TalentMapApplication.java
@@ -0,0 +1,13 @@
+package dev.royaumendi.talentmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMapApplication.class, args);
+	}
+
+}
